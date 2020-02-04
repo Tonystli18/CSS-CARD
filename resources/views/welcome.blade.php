591dd3639672a18788e7788971b124639a35422b
Tonystli18 <34615551+Tonystli18@users.noreply.github.com>
2020-02-04 11:48:05 -0800

    Use traditional naming and CDN flex to create a card

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/app.css">
+    <link rel="stylesheet" href="./css/normalize.css">
+
+</head>
+
+<body>
+    <div class="card">
+        <div class="card-left">
+            <a href="#" class="card-skill-button">PHP</a>
+            <img src="./images/customer-support.svg">
+            <span class="card-difficulty">Intermediate Difficulty</span>
+        </div>
+
+        <div class="card-right">
+            <h3 class="card-title"><a href="#">Testing Jargon</a></h3>
+            <p class="card-excerpt">
+                Lorem ipsum dolor sit amet consectetur r elit. Illum, quisquam quo? Exercitationem reiciendis amet est qui sapiente pariatur earum, quidem
+            </p>
+            <div class="card-meta">
+                <div>
+                    <img src="./images/books-stack-of-three.svg" >
+                    5 Lessons
+                </div>
+                <div>
+                    <img src="./images/clock.svg">
+                    53:10 minutes
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
